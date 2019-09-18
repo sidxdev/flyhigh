5dba08c38d793b3a0424a83d2eea9e1b0ed35a6e
Siddharth Srivastava <siddharth.srivastava@sap.com>
2019-09-18 03:45:58 +0000

    vendor passenger discounts

--- flyhigh_ui/webapp/controller/VendorPassenger.controller.js
+++ flyhigh_ui/webapp/controller/VendorPassenger.controller.js
@@ -23,7 +23,52 @@ sap.ui.define([
 		},
 
 		onAddDiscount: function (oEvent) {
+			var oTable = that.getView().byId("passengerTable");
+			var sItemPath = oEvent.getSource().getParent().getBindingContextPath();
+			var sFlightOperator = oTable.getModel().getProperty(sItemPath + "/operator");
+			var sFlightFlightNum = oTable.getModel().getProperty(sItemPath + "/flightnum");
+			var sFlightDateTime = oTable.getModel().getProperty(sItemPath + "/datetime");
 
+			that._getAddDiscountsDialog().open();
+
+			var oFlightDetail = sap.ui.getCore().byId("addVendorPassengerDiscountFlightDetail");
+			var oFlightDate = sap.ui.getCore().byId("addVendorPassengerDiscountDate");
+			var oCatalogTable = sap.ui.getCore().byId("addVendorPassengerDiscountTableContainer");
+			oFlightDetail.setText(sFlightOperator + sFlightFlightNum);
+			oFlightDate.setText(sFlightDateTime);
+
+			// Load Catalog
+			oBusyDialog.open();
+			Service.get("/api/vendor/catalog").then(function (oData) {
+				oData.data = that._aggregateDiscounts(oData.data);
+				oCatalogTable.setModel(new JSONModel(oData));
+				oCatalogTable.bindItems("/data", that._tableCatalogRowTemplate());
+			}).catch(function () {}).finally(function () {
+				oBusyDialog.close();
+			});
+		},
+
+		_getAddDiscountsDialog: function () {
+			if (!that._oAddDiscountsDialog) {
+				that._oAddDiscountsDialog = sap.ui.xmlfragment("flyhigh.flyhigh_ui.fragment.vendorAddDiscountPax", that);
+			}
+			return that._oAddDiscountsDialog;
+		},
+
+		_destroyAddDiscountsDialog: function () {
+			if (that._oAddDiscountsDialog) {
+				that._oAddDiscountsDialog.destroy();
+				that._oAddDiscountsDialog = null;
+			}
+		},
+
+		addVendorPassengerDiscountDialogSave: function (oEvent) {
+			// TODO
+			that._destroyAddDiscountsDialog();
+		},
+
+		addVendorPassengerDiscountDialogCancel: function (oEvent) {
+			that._destroyAddDiscountsDialog();
 		},
 
 		_fetchPassengers: function () {
@@ -43,10 +88,7 @@ sap.ui.define([
 				type: "Active",
 				cells: [
 					new sap.m.Text({
-						text: "{operator}{flightnum}"
-					}),
-					new sap.m.Text({
-						text: "{direction}"
+						text: "{operator}{flightnum} {direction}"
 					}),
 					new sap.m.Text({
 						text: "{datetime}"
@@ -58,6 +100,25 @@ sap.ui.define([
 						icon: "sap-icon://add",
 						text: "Discount",
 						press: that.onAddDiscount
+					})
+				]
+			});
+		},
+
+		_tableCatalogRowTemplate: function () {
+			return new sap.m.ColumnListItem({
+				cells: [
+					new sap.m.Text({
+						text: "{model}"
+					}),
+					new sap.m.Text({
+						text: "{category}"
+					}),
+					new sap.m.Text({
+						text: "{retailPrice}"
+					}),
+					new sap.m.Text({
+						text: "{description}"
 					})
 				]
 			});
